dd88904d3723adb4200c3c6c777016b4f85f8772
Arunkumar-SM <60097164+Arunkumar-SM@users.noreply.github.com>
2020-03-03 16:10:05 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestApp_Partial.Startup))]
+namespace TestApp_Partial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
